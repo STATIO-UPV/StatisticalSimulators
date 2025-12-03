f9b88f2608afc32745d6ccf2f215d562eb8caddf
Julia <juliadomingo13.5@gmail.com>
2025-12-03 21:47:00 +0100

    Diseño de plantilla final

--- template/appr/app.R
+++ template/appr/app.R
@@ -30,55 +30,68 @@ texts <- list(
   title = c( 
     ES = "Título de tu aplicación", 
     EN = "Application Title", 
-    VAL = "Títol de la teua aplicació" ), 
+    VAL = "Títol de la teua aplicació" 
+    ), 
   explanation = c( 
     ES = "Breve explicación de la aplicación.", 
     EN = "Short explanation of the application.", 
-    VAL = "Breu explicació de l'aplicació." ), 
+    VAL = "Breu explicació de l'aplicació." 
+    ), 
   button_parameters = c( 
     ES = "Parámetros", 
     EN = "Parameters", 
-    VAL = "Paràmetres" ), 
+    VAL = "Paràmetres" 
+    ), 
   text_downmenu= c( 
     ES = "1. Tu menú desplegable", 
     EN= "2. Your dropdown menu", 
-    VAL= "3. El teu menú desplegable" ), 
+    VAL= "3. El teu menú desplegable" 
+    ), 
   dropdown_label = c( 
     ES = "Título del menú desplegable", 
     EN = "Title of drop-down menu", 
-    VAL = "Títol del menú desplegable" ), 
+    VAL = "Títol del menú desplegable" 
+    ), 
   option1 = c( 
     ES = "OPCIÓN 1", 
     EN = "OPTION 1", 
-    VAL = "OPCIÓ 1" ), 
+    VAL = "OPCIÓ 1" 
+    ), 
   option2 = c( 
     ES = "OPCIÓN 2", 
     EN = "OPTION 2", 
-    VAL = "OPCIÓ 2" ), 
+    VAL = "OPCIÓ 2" 
+    ), 
   slider1= c( 
     ES = "BARRA DESLIZANTE 1", 
     EN= "SLIDER 1", 
-    VAL= "Barra lliscant 1" ), 
+    VAL= "Barra lliscant 1" 
+    ), 
   slider2= c( 
     ES = "BARRA DESLIZANTE 2", 
     EN= "SLIDER 2", 
-    VAL= "Barra lliscant 2" ), 
+    VAL= "Barra lliscant 2" 
+    ), 
   slider3= c( 
     ES = "BARRA DESLIZANTE 3", 
     EN= "SLIDER 3", 
-    VAL= "Barra lliscant 3" ), 
+    VAL= "Barra lliscant 3" 
+    ), 
   slider4= c( 
     ES = "BARRA DESLIZANTE 4", 
     EN= "SLIDER 4", 
-    VAL= "Barra lliscant 4" ), 
+    VAL= "Barra lliscant 4" 
+    ), 
   slider5= c( 
     ES = "BARRA DESLIZANTE 5", 
     EN= "SLIDER 5", 
-    VAL= "Barra lliscant 5" ), 
+    VAL= "Barra lliscant 5" 
+    ), 
   slider6= c( 
     ES = "BARRA DESLIZANTE 6", 
     EN= "SLIDER 6", 
-    VAL= "Barra lliscant 6" ), 
+    VAL= "Barra lliscant 6" 
+    ), 
   panel1 = c( 
     ES = "Panel 1", 
     EN = "Panel 1", 
@@ -86,15 +99,18 @@ texts <- list(
   panel2 = c( 
     ES = "Panel 2", 
     EN = "Panel 2", 
-    VAL = "Panell 2" ), 
+    VAL = "Panell 2" 
+    ), 
   panel3 = c( 
     ES = "Panel 3", 
     EN = "Panel 3", 
-    VAL = "Panell 3" ), 
+    VAL = "Panell 3" 
+    ), 
   plot= c(
     ES= "Gráfico",
     EN= "Plot", 
-    VAL= "Gràfic"),
+    VAL= "Gràfic"
+    ),
   plot_x= c(
     ES= "Valor_X",
     EN= "X_value",
@@ -110,25 +126,21 @@ texts <- list(
     EN = "Example of data (means, std):",
     VAL = "Exemple de dades (mitjanes, desviació típica):"
   ),
-  
   equation_label = c(
     ES = "Ecuación",
     EN = "Equation",
     VAL = "Equació"
   ),
-  
   tabla= c(
     ES= "Tabla Ejemplo: Este es un ejemplo de tabla (para ANOVA)", 
     EN= "Example Table: This is a table example (for ANOVA)", 
     VAL= "Taula Exemple: Esta és una taula d'exemple (per a ANOVA)" 
   ),
-  
   interpretation= c(
     ES= "Interpretación", 
     EN= "Interpretation", 
     VAL= "Interpretació"
   ),
-  
   conclusion1= c(
     ES= "Esta es una de las conclusiones", 
     EN= "This is one of the conclussions", 
@@ -150,11 +162,12 @@ texts <- list(
     EN= "STATIO is an Educational Innovation and Improvement Project (PIME/25-26/562) 
     developed by the DEIOAC-UPV.", 
     VAL= "STATIO és un Projecte d'Innovació i Millora Educativa (*PIME/25-26/562) 
-    desenrotllat pel *DEIOAC-UPV" )
+    desenrotllat pel *DEIOAC-UPV" 
+    )
 )
 
 
-# Función de traducción: dado el ID y el idioma, devuelve el texto correcto
+# Función de traducción: dado el ID y el idioma, devuelve el texto correcto. NO MODIFICAR.
 
 tr <- function(id, lang) { texts[[id]][[lang]] }
 
@@ -162,9 +175,9 @@ tr <- function(id, lang) { texts[[id]][[lang]] }
 
 ui <- fluidPage(
   
-  useShinyjs(), # Activa funciones JS. 
+  useShinyjs(), # Activa funciones JS. NO MODIFICAR.
   
-  # BOTONES PARA CAMBIAR DE IDIOMA EN LA PÁGINA. No tocarlos. 
+  # BOTONES PARA CAMBIAR DE IDIOMA EN LA PÁGINA. NO MODIFICARlOS. 
   
   absolutePanel(
     top = 10, right = 10, fixed = TRUE,
@@ -174,7 +187,7 @@ ui <- fluidPage(
   ),
   
   # ---------------------------------------------------------------------------
-  # 1. CSS PARA LA ESTRUCTURA VISUAL DE LA TEMPLATE. NO TOCAR. 
+  # 1. CSS PARA LA ESTRUCTURA VISUAL DE LA TEMPLATE. NO MODIFICAR. 
   
   tags$head(
     tags$style(HTML("
@@ -272,15 +285,15 @@ ui <- fluidPage(
     }
   "))
   ),
-  # -------------------------------FIN DEL CSS------------------------------------
+  # -------------------------------FIN DEL CSS---------------------------------
   
-  # Botón que abre/cierra el panel lateral. No tocar.
+  # Botón que abre/cierra el panel lateral. No MODIFICAR.
   
   if(showparams) {
     actionButton("toggleSidebar", textOutput("button_parameters"))
   },
   
-  # PANEL LATERAL DE PARÁMETROS.
+  # PANEL LATERAL DE PARÁMETROS. SE PUEDE MODIFICAR. 
   
   div(id="sidebarWrapper",
       style="padding-top: 50px;",
@@ -312,9 +325,9 @@ ui <- fluidPage(
   
   div(id="contentWrapper", class = "shifted",
       
-      # Título y explicación. No modificar aquí, solo en el diccionario. 
+      # Título y explicación. NO MODIFICAR aquí, solo en el diccionario. 
       
-        div(style="padding-top:70px;",
+        div(style="padding-top:50px; margin-bottom:30px",
             h2(textOutput("title"), align="center"),
             div(style="display:flex; justify-content:center;",
                 div(style="border:2px solid #4a90e2; border-radius:12px; padding:12px; 
@@ -326,12 +339,15 @@ ui <- fluidPage(
       
       # --------------------- TABS DE LA APLICACIÓN ---------------------
       
+      # Se puede modificar. 
+      
       tabsetPanel(
         tabPanel(textOutput("panel1_title"),
                  htmlOutput("plot_title"),
                  plotOutput("Plot_ID"),
                  uiOutput("sampleStats"),
                  htmlOutput("table_name"), verbatimTextOutput("aov"),
+                 textOutput("pValueText"), 
                  htmlOutput("interpretation_text"), textOutput("conclusionText"),
                  uiOutput("resultsMessage")
         ),
@@ -341,7 +357,8 @@ ui <- fluidPage(
         tabPanel("Data", tableOutput("data"))
       ),
       
-      div(style="margin-top:40px; text-align:center;",
+      # Créditos y logos. NO MODIFICAR. 
+      div(style="margin-top:40px; text-align:center;margin-bottom:40px;",
           tags$img(src='DEIOAC.png', height=80),
           tags$img(src='UPV.png', height=80),
           htmlOutput("creditos")
@@ -361,24 +378,29 @@ server <- function(input, output) {
     shinyjs::runjs("setTimeout(function() { $(window).trigger('resize'); }, 350);")
   })
   
-  # ---------------- IDIOMA -----------------
+  # ---------------- IDIOMA. NO MODIFICAR. -----------------
   language <- reactiveVal("ES") 
   observeEvent(input$lang_es, { language("ES") }) 
   observeEvent(input$lang_en, { language("EN") }) 
   observeEvent(input$lang_va, { language("VAL") })
   
-  # ---------------------- TEXTOS TRADUCIBLES -------------------------
+  # -------- TEXTOS TRADUCIBLES. SE PUEDEN AÑADIR/ELIMINAR SI HACEN FALTA. ----
   
   output$title <- renderText({ tr("title", language()) })
-  output$explanation <- renderUI({ HTML(tr("explanation", language())) })
+  output$explanation <- renderUI({HTML(tr("explanation", language())) })
   output$panel1_title <- renderText({ tr("panel1", language()) })
   output$panel2_title <- renderText({ tr("panel2", language()) })
   output$panel3_title <- renderText({ tr("panel3", language()) })
   output$button_parameters <- renderText({tr("button_parameters", language())})
   output$text_downmenu <- renderText({ tr("text_downmenu", language()) })
   output$creditos <- renderUI({ HTML(tr("credits", language())) })
+  output$plot_title <- renderUI({
+    HTML(paste0("<h3 style='font-size:22px; font-weight:bold'>",
+                tr('plot', language()),
+                "</h3>"))
+  })
   
-  # ---------------- PARÁMETROS DINÁMICOS ----------------
+  # ---------------- PARÁMETROS DINÁMICOS. SE PUEDEN AÑADIR/ELIMINAR SI HACEN FALTA. ----------------
   
   output$dropdown_ui <- renderUI({ 
     selectInput( "server_id", tr("dropdown_label", language()), 
@@ -387,29 +409,7 @@ server <- function(input, output) {
                    c(tr("option1", language()), tr("option2", language())) ) ) }
   )
   
-  # Sliders generados dinámicamente
-  
-  output$plot_title <- renderUI({
-    HTML(paste0("<h3 style='font-size:22px; font-weight:bold'>",
-                tr('plot', language()),
-                "</h3>"))
-  })
-  
-  output$table_name <- renderUI({
-    HTML(paste0("<h3 style='font-size:22px; font-weight:bold'>",
-                tr('tabla', language()),
-                "</h3>"))
-  })
-  
-  output$interpretation_text <- renderUI({
-    HTML(paste0("<h3 style='font-size:22px; font-weight:bold'>",
-                tr('interpretation', language()),
-                "</h3>"))
-  })
-  
-  
-  
-  
+  # Sliders generados dinámicamente. SE PUEDEN AÑADIR/ELIMINAR SI HACEN FALTA.
   
   output$slider1_ui <- renderUI({
     sliderInput("sliderId", tr("slider1", language()), min = 1, max = 100, value = 30)
@@ -420,6 +420,7 @@ server <- function(input, output) {
   output$slider3_ui <- renderUI({
     sliderInput("sliderId3", tr("slider3", language()), min = 1, max = 100, value = 30)
   })
+  
   
   output$slider4_ui <- renderUI({
     sliderInput("sliderId4", tr("slider4", language()), min = 1, max = 20, value = 10)
@@ -432,20 +433,12 @@ server <- function(input, output) {
   })
   
   
-  
-  # Reactive expression to generate the requested distribution ----
-  # This is called whenever the inputs change. The output functions
-  # defined below then use the value computed from this expression
-  
-  ##################### DATASET CREATION ####################
-  
-  #Set as many parameters as selected. EXAMPLE OF HOW A DATASET COULD BE CALCULED. Set the parameters/values as you want. 
-  #This generates a randomized dataset based on means and std set in sliders or fixed. Also, you can load your own dataset.
-  # You can modify the way the dataset is created, this is an example. 
+  # -------------------- GENERACIÓN DE DATOS -------------------------
+  # Dataset generado pseudoaleatoriamente. Modificar con dataset propio si se desea. 
   
   d <- reactive({
-    n <- 50 #Number of participants generated in the dataset.
-    uA <- input$sliderId # This is how to link parameters to the dataset generation. 
+    n <- 50 # Número de individuos en el dataset
+    uA <- input$sliderId # Linkeamos ciertos parámetros a los sliders.  
     uB <- input$sliderId2
     uC <- input$sliderId3
     sdA <- 10
@@ -454,106 +447,53 @@ server <- function(input, output) {
     
     set.seed(123)
     
-    # FIX BUG 3: Changed groups from "a", "b", "c" to "A", "B", "C"
-    # so they match the summarization logic below (means$group == "A")
-    example1 <- data.frame(
-      group = "A",
-      value = rnorm(n, uA, sdA)
+    df <- bind_rows(
+      data.frame(group="A", value=rnorm(n, uA, 10)),
+      data.frame(group="B", value=rnorm(n, uB, 10)),
+      data.frame(group="C", value=rnorm(n, uC, 10))
     )
-    
-    example2 <- data.frame(
-      group = "B",
-      value = rnorm(n, uB, sdB)
-    )
-    
-    example3 <- data.frame(
-      group = "C",
-      value = rnorm(n, uC, sdC)
-    )
-    
-    bind_rows(example1, example2, example3)
+    df
   })
   
-  ##################### PLOTS  ##################### 
-  
-  #Set plots parameters. This is an example of a plot that can be created.
-  # HERE, YOU WILL SHOW, IF NEEDED, ANY PLOT YOU WANT IN YOUR SIMULATION. 
-  plot_x_label <- reactive({tr("plot_x" ,language())})
-  plot_y_label <- reactive({tr("plot_y", language())})
+  # ----------------------------- PLOT(S) ------------------------------------
   
   output$Plot_ID <- renderPlot({
-    data <- d()
-    ggplot(data, aes(x = group, y = value, fill = group)) +
-      geom_violin(trim = FALSE) +
-      geom_jitter(width = 0.1, size = 0.5) +
-      labs(
-        x = plot_x_label(),
-        y = plot_y_label()
-      ) +
+    ggplot(d(), aes(group, value, fill=group)) +
+      geom_violin(trim=FALSE) +
+      geom_jitter(width=0.1, size=0.6) +
+      labs(x=tr("plot_x", language()), y=tr("plot_y", language())) +
       theme_minimal()
   })
   
   
-  ######## DATA DESCRIPTION #################### 
-  
-  #In the variable sample stats we save: data, means per group, and a message formatted that shows data (means or std)
+  # ---------------------- ESTADÍSTICAS -------------------------
   
   output$sampleStats <- renderUI({
-    data <- d()
-    means <- data %>% group_by(group) %>% summarise(mean = mean(value))
-    
-    # Here, you can add as many equations or parameters as needed in your application. Maybe you would want to add ranges,
-    # or max values, or medians. 
+    means <- d() %>% group_by(group) %>% summarise(mean = mean(value))
     
     withMathJax(HTML(paste0(
-      "<h5>", tr("sampleStats_title", language()), "</h5><br>",
-      "$$", tr("equation_label", language()), ":\\quad ",
-      "Eq1 = ", round(means$mean[means$group == "A"], 2), ",\\quad ",
-      "Eq2 = ", round(means$mean[means$group == "B"], 2), ",\\quad ",
-      "Eq3 = ", round(means$mean[means$group == "C"], 2), "$$"
+      "<h5>", tr("sampleStats_title", language()), "</h5>",
+      "$$ \\text{Media A}=", round(means$mean[1],2),
+      ",\\; \\text{Media B}=", round(means$mean[2],2),
+      ",\\; \\text{Media C}=", round(means$mean[3],2), "$$"
     )))
   })
+  # ---------------------- EJEMPLO ------------------------- 
   
-  ##################### SIMULATION. TEST YOU WANT TO SHOW ##################### 
-  
-  # Reactive example (ANOVA, T-test, etc.). Reactive means that it depends on d() data. Is the way to vincule
-  # data to a showed output. 
-  
-  # Change anova with the test you want to create. Change "aov" with "t.test" (test t), 
-  # "wilcox.test" (wilcoxon), etc. 
-  
-  #ANOVA IS CREATED TO SHOW AN EXAMPLE.
-  # HERE, YOU CAN ADD AS MANY RESULTS, CONCLUSSIONS OR EXPLANATIONS YOU WANT IN YOUR SIMULATION.
-  # Also, you can remove whatever you want. 
-  # ALSO, YOU CAN SEND ANY OF YOUR RESULTS TO A DIFFERENT TAB. 
-  
-  test_result <- reactive({
-    data <- d() #Data used in your test. 
-    aov(value ~ group, data = data)
+  output$table_name <- renderUI({
+    HTML(paste0("<h3 style='font-size:22px; font-weight:bold'>",
+                tr('tabla', language()),
+                "</h3>"))
   })
   
-  # Get the p-value in ANOVA. Change this with your test (if p-val is not needed, 
-  # remove this and change it into your results).
+  test_result <- reactive(aov(value ~ group, data=d()))
+  output$aov <- renderPrint(summary(test_result()))
   
-  p_value <- reactive({
-    anova_summary <- summary(test_result())
-    anova_summary[[1]][["Pr(>F)"]][1]
+  p_value <- reactive(summary(test_result())[[1]][["Pr(>F)"]][1])
+  
+  output$conclusionText <- renderText({
+    if (p_value() < 0.05) tr("conclusion1", language()) else tr("conclusion2", language())
   })
-  
-  # Conclussion message based on pvalue. Maybe you want conclussions or results 
-  # based on other parameter or only an explanation. 
-  
-  conclusion <- reactive({
-    if (p_value() < 0.05) {
-      tr("conclusion1", language())
-    } else {
-      tr("conclusion2", language())
-    }
-  })
-  
-  
-  # This section is to set outputs, important to show what u want. 
-  # Add as many outputs as you want to show (p-valor, confidences, conclusions ...)
   
   output$aov <- renderPrint({
     anova_result <- test_result()
@@ -564,19 +504,22 @@ server <- function(input, output) {
     paste("p-value or other parameter: ", format(p_value(), digits = 5))
   })
   
-  output$conclusionText <- renderText({
-    conclusion()
+  output$interpretation_text <- renderUI({
+    HTML(paste0("<h3 style='font-size:22px; font-weight:bold'>",
+                tr('interpretation', language()),
+                "</h3>"))
   })
+
   
-  # FIX BUG 1: Used tr() for translation
-  output$resultsMessage <- renderUI({
-    HTML(paste0("<h3 style='color: gray; text-align: center;'>", tr("resultsMessage", language()), "</h3>"))
-  })
-  
+  # DATOS DEL PANEL 4
   output$data <- renderTable({
     d()
   })
   
+# ELIMINAR ESTO CUANDO SE HAGA LA TEMPLATE. 
+  output$resultsMessage <- renderUI({
+    HTML(paste0("<h3 style='color: gray; text-align: center;'>", tr("resultsMessage", language()), "</h3>"))
+  })
 }
 
 # Create Shiny app ----
